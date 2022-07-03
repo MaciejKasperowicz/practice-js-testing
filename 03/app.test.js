f3bbb0707695f601bb767a1dc10fa0e36311d675
MaciejKasperowicz <makasp@wp.pl>
2022-07-03 16:51:33 +0200

    4 test completed

--- 03/app.test.js
+++ 03/app.test.js
@@ -19,10 +19,18 @@ it("throw exception when property max is not a number", () => {
 })
 
 it("throw exception when property min is greater than property max", () => {
-
     function isMinGreaterThanMax() {
         randomNumber(3, 1)
     }
-
     expect(isMinGreaterThanMax).toThrow()
+})
+
+it("is returned value between min and max property", () => {
+    const min = 5;
+    const max = 10;
+
+    console.log(randomNumber(min, max))
+    expect(randomNumber(min, max)).toBeGreaterThanOrEqual(min);
+    expect(randomNumber(min, max)).toBeLessThanOrEqual(max);
+
 })
