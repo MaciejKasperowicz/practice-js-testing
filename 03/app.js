ee9b78eb668c20c8b44b678d23eddaef299354af
MaciejKasperowicz <makasp@wp.pl>
2022-07-03 14:49:05 +0200

    2 test passed

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,9 @@
 export default function randomNumber(min, max) {
+    if (typeof min !== "number") {
+        throw new Error("Property min have to be a number");
+    }
+    if (typeof max !== "number") {
+        throw new Error("Property max have to be a number");
+    }
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,19 @@ import randomNumber from './app';
 it("return 1 when you draw number between 1 and 1", () => {
     expect(randomNumber(1, 1)).toBe(1);
 })
+
+it("throw exception when property min is not a number", () => {
+    function isMinPropNumber() {
+        randomNumber(5, undefined);
+    }
+
+    expect(isMinPropNumber).toThrow()
+})
+
+it("throw exception when property max is not a number", () => {
+    function isMaxPropNumber() {
+        randomNumber(undefined, 10);
+    }
+
+    expect(isMaxPropNumber).toThrow()
+})
